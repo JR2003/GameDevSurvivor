1838b84eff01aea5d8e3ffba0ede60ed5ba22ffb
benib0b <fries.ben@gmx.de>
2025-01-05 14:47:46 +0100

    menu +  slime

---
+++ gamedevsurvivor/Menu/menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_play_button_pressed():
+	print("ich will spielen")
+	# Replace with function body. -> put Game Scene
+
+func _on_exit_button_pressed():
+	get_tree().quit()
