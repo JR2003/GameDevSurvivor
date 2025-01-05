0f6a04304e88aa1327cf2cac7230a8823156043a
benib0b <fries.ben@gmx.de>
2025-01-05 15:20:34 +0100

    menu connected to play scene

--- gamedevsurvivor/Menu/menu.gd
+++ gamedevsurvivor/Menu/menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_play_button_pressed():
 	print("ich will spielen")
-	# Replace with function body. -> put Game Scene
+	get_tree().change_scene_to_file("res://world_2d.tscn")
 
 func _on_exit_button_pressed():
 	get_tree().quit()
