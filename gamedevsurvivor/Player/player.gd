025d364ce5c6de0d670e40d6badcf9638333dd5f
JR2003 <j.rudolph2003@gmail.com>
2025-01-07 12:26:18 +0100

    exp balken der mit level und exp arbeitet

--- gamedevsurvivor/Player/player.gd
+++ gamedevsurvivor/Player/player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 @onready var anim = $AnimatedSprite2D  # Sprite-Node
+@onready var xp_bar = $Control/ProgressBar# XP-Bar Referenz (Pfad anpassen)
 @export var is_shooting = false
+@onready var label = $Control/ProgressBar/Label
 var last_direction = Vector2(0, 1)  # Standardmäßig nach unten
 var exp = 0
 var level = 1
@@ -10,6 +12,7 @@ var weapon = null
 
 func _ready() -> void:
 	weapon = get_node("Weapon2")
+	update_exp_label()
 
 func _physics_process(delta):
 	handle_input()
@@ -29,16 +32,14 @@ func handle_input():
 		velocity = Vector2.ZERO
 
 	# Richtung zur Maus aktualisieren
-	
 	var mouse_direction = (get_global_mouse_position() - global_position).normalized()
 	last_direction = mouse_direction
 		
-
 func update_animation():
-		if velocity == Vector2.ZERO:
-			anim.play("idle")
-		else:
-			play_walk_animation(last_direction)
+	if velocity == Vector2.ZERO:
+		anim.play("idle")
+	else:
+		play_walk_animation(last_direction)
 
 func play_walk_animation(direction: Vector2):
 	if direction.x < -0.5:
@@ -55,8 +56,15 @@ func play_walk_animation(direction: Vector2):
 func gain_exp(amount: int) -> void:
 	exp += amount
 	print("EXP erhalten: ", amount, "| Gesamte EXP: ", exp)
+
+	# XP-Bar aktualisieren
+	xp_bar.value = exp
+	
 	if exp >= exp_to_next_level:
 		level_up()
+		update_exp_label()
+	else:
+		update_exp_label()
 
 func level_up() -> void:
 	weapon.upgrade()
@@ -64,3 +72,9 @@ func level_up() -> void:
 	exp -= exp_to_next_level
 	exp_to_next_level *= 1.5
 	print("Level-Up! Neues Level: ", level)
+
+	# XP-Bar zurücksetzen oder aktualisieren
+	xp_bar.value = exp  # Setze die XP-Bar auf den neuen EXP-Wert
+
+func update_exp_label() -> void:
+	label.text = str(exp) + " / " + str(exp_to_next_level)  # Setze den Text im Label
