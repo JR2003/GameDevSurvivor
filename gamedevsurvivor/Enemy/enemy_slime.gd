1935410a7ba924b1155b71cc19da50644664a683
JR2003 <j.rudolph2003@gmail.com>
2025-01-07 17:24:27 +0100

    hallo

---
+++ gamedevsurvivor/Enemy/enemy_slime.gd
@@ -0,0 +1,65 @@
+extends CharacterBody2D
+
+var chase = false
+var player = null
+var speed = 50
+var health = 100
+var alive = true
+var exp_reward = 50  # EXP, die dieser Gegner gibt
+var hurt = false
+
+func _ready() -> void:
+	player = get_parent().get_node("Player")
+	
+		
+
+func _on_detection_range_body_entered(body: CharacterBody2D) -> void:
+	if body == player:
+		chase = true
+
+func _on_detection_range_body_exited(body: CharacterBody2D) -> void:
+	
+	chase = false
+
+func _physics_process(delta: float) -> void:
+	if !alive or hurt:
+		return
+	if chase:
+		position += (player.position - position) / speed
+		$AnimatedSprite2D.play("move")
+		if (player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = false
+		else:
+			$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.play("idle")
+
+func die():
+	give_exp_to_player()
+	alive = false
+	$AnimatedSprite2D.play("death")
+	await $AnimatedSprite2D.animation_looped
+	
+	queue_free()
+
+func give_exp_to_player() -> void:
+	if player == null:
+		print("player nicht da")
+	elif !player.has_method("gain_exp"):
+		print("method nicht geunden")
+	if player and player.has_method("gain_exp"):
+		player.gain_exp(exp_reward)
+
+func _on_damage_hitbox_area_entered(area: Area2D) -> void:
+	
+	
+	if !area.is_in_group("slime_hitbox") and !area.is_in_group("player"):
+		health -= 10
+		hurt = true
+		print("hit")
+		$AnimatedSprite2D.play("hurt")
+		await $AnimatedSprite2D.animation_looped
+		
+		hurt = false
+		if health <= 0:
+			die()
---
+++ gamedevsurvivor/world_2d.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+# Gegner-Szenen als PackedScene-Variablen
+@export var enemy_slime_scene: PackedScene
+@export var enemy_skeleton_scene: PackedScene
+
+# Bereich, in dem die Gegner gespawnt werden
+@export var spawn_area: Rect2
+
+func _ready():
+	# Verbinde das Signal des Timers
+	$Timer.timeout.connect(spawn_enemy)
+
+func spawn_enemy():
+	# Zufällig zwischen den beiden Gegnertypen wählen
+	var enemy_scene = enemy_slime_scene if randf() < 0.5 else enemy_skeleton_scene
+
+	# Einen neuen Gegner instanzieren
+	var enemy_instance = enemy_scene.instantiate()
+
+	# Zufällige Position innerhalb des Spawn-Bereichs berechnen
+	var spawn_position = Vector2(
+		randf_range(-1000, 1000),
+		randf_range(0,500)
+	)
+	print(spawn_position)
+	enemy_instance.position = spawn_position
+
+	# Gegner in die Szene hinzufügen
+	add_child(enemy_instance)
