dbe2f80fff2dc077a5fe979fb545a5da6dd605bf
benib0b <fries.ben@gmx.de>
2025-01-07 20:14:56 +0100

    arrow hits enemy

--- gamedevsurvivor/Arrow.gd
+++ gamedevsurvivor/Arrow.gd
@@ -14,11 +14,5 @@ func _physics_process(delta):
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemies"):
-		print("arrow pierced enemy")
-		queue_free()
-
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("enemies"):
-		print("arrow pierced enemyss")
+		print("arrow hit enemy")
 		queue_free()
